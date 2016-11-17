98cbf72c9306ffb2b4326f6dbe126be9472573d7
rhodgman <ryan.g.hodgman@gmail.com>
2016-11-17 14:29:44 -0500

    Broadcast Receiver

--- NXTRemoteControl-Part1/app/src/main/java/com/cyberdynesystems/nxtremotecontrol_v0/MainActivity.java
+++ NXTRemoteControl-Part1/app/src/main/java/com/cyberdynesystems/nxtremotecontrol_v0/MainActivity.java
@@ -1,7 +1,8 @@
 package com.cyberdynesystems.nxtremotecontrol_v0;
 
+import android.content.BroadcastReceiver;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
@@ -20,14 +21,15 @@ import com.roughike.bottombar.OnTabSelectListener;
 public class MainActivity extends AppCompatActivity {
 
     //Instance variables
-    Button cv_connectBtn, closeBtn;
+    Button cv_connectBtn, cv_closeBtn;
     ImageView bluetoothIcon;
     TextView cv_connectionStatus, cv_deviceName;
-    RobotController controller;
+    RobotController cv_robotController;
     BottomBar bottomBar;
     TextView BatteryPercent;
     ProgressBar cv_batteryLevel;
     Double batteryPercentage;
+    BroadcastReceiver cv_btMonitor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,11 +41,12 @@ public class MainActivity extends AppCompatActivity {
         //Initialize variables/Views
         cv_connectionStatus = (TextView) findViewById(R.id.tv_connectionStatus);
         cv_deviceName = (TextView) findViewById(R.id.vv_tvDeviceName);
-        controller = RobotController.getRobotController(MainActivity.this);
+        cv_robotController = RobotController.getRobotController(MainActivity.this);
+        cv_btMonitor = cv_robotController.cf_getBTMonitor();
         bottomBar = (BottomBar) findViewById(R.id.bottomBar);
         bluetoothIcon = (ImageView) findViewById(R.id.iv_bluetooth);
         cv_connectBtn = (Button) findViewById(R.id.cv_connectBtn);
-        closeBtn = (Button) findViewById(R.id.closeBtn);
+        cv_closeBtn = (Button) findViewById(R.id.closeBtn);
         BatteryPercent = (TextView) findViewById(R.id.tv_BatteryPercent);
         cv_batteryLevel = (ProgressBar) findViewById(R.id.batteryLevelProgess);
 
@@ -60,22 +63,22 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(lv_intent);
                     overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_right);
                 }
-
             }
         });
 
         //Check if robot is already connected
-        if (controller.getConnectionStatus()) {
-            closeBtn.setEnabled(true);
+        if (cv_robotController.getConnectionStatus()) {
+            cv_closeBtn.setEnabled(true);
             cv_connectBtn.setEnabled(false);
             bluetoothIcon.setImageResource(R.drawable.ic_connected_bluetooth);
             cv_connectionStatus.setText("Connected");
-            batteryPercentage = controller.cf_battery();
+            batteryPercentage = cv_robotController.cf_battery();
             BatteryPercent.setText(batteryPercentage + "%");
             cv_batteryLevel.setProgress(batteryPercentage.intValue());
-            cv_deviceName.setText("Connected to: " + controller.getRobotName());
-        } else {
-            closeBtn.setEnabled(false);
+            cv_deviceName.setText("Connected to: " + cv_robotController.getRobotName());
+        }
+        else {
+            cv_closeBtn.setEnabled(false);
             cv_connectBtn.setEnabled(true);
             bluetoothIcon.setImageResource(R.drawable.ic_disconnected_bluetooth);
             cv_connectionStatus.setText("Not connected");
@@ -87,27 +90,38 @@ public class MainActivity extends AppCompatActivity {
         cv_connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public synchronized void onClick(View v) {
-                boolean done = controller.cf_findRobot(MainActivity.this);
+                boolean done = cv_robotController.cf_findRobot(MainActivity.this);
                 if (done)
                     updateConnectScreen();
             }
         });
 
         //Close button listener
-        closeBtn.setOnClickListener(new View.OnClickListener() {
+        cv_closeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public synchronized void onClick(View v) {
-                controller.handleDisconnected();
+                cv_robotController.cf_handleDisconnected();
                 cv_connectBtn.setEnabled(true);
-                closeBtn.setEnabled(false);
+                cv_closeBtn.setEnabled(false);
                 bluetoothIcon.setImageResource(R.drawable.ic_disconnected_bluetooth);
                 BatteryPercent.setText("0.0%");
                 cv_batteryLevel.setProgress(0);
                 cv_connectionStatus.setText("Not Connected");
             }
         });
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(cv_btMonitor, new IntentFilter("android.bluetooth.device.action.ACL_CONNECTED"));
+        registerReceiver(cv_btMonitor, new IntentFilter("android.bluetooth.device.action.ACL_DISCONNECTED"));
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(cv_btMonitor);
     }
 
     @Override
@@ -126,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void updateConnectScreen() {
-        closeBtn.setEnabled(true);
+        cv_closeBtn.setEnabled(true);
         cv_connectBtn.setEnabled(false);
         bluetoothIcon.setImageResource(R.drawable.ic_connected_bluetooth);
         cv_connectionStatus.setText("Connected");
