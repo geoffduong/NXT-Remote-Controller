a7449804c731e9dc92445a1bbfb9b3019c9bb725
mrricearoni <duongg4848@gmail.com>
2016-11-21 15:13:09 -0500

    Started layout for Poll Intent ADDED
    Images: distance, light, servo, sound, touch
    Java Classes: PollIntent.java
    Layout: poll_intent.xml, poll_listcell.xml

---
+++ NXTRemoteControl-Part1/app/src/main/java/com/cyberdynesystems/nxtremotecontrol_v0/PollIntent.java
@@ -0,0 +1,17 @@
+package com.cyberdynesystems.nxtremotecontrol_v0;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by geoffduong on 11/21/16.
+ */
+
+public class PollIntent extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.poll_intent);
+    }
+}
